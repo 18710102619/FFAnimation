7ec8abc6729fbe1b4b5eb85a4982900a66f03653
张玲玉 <2949820345@qq.com>
2017-02-10 10:39:09 +0800

    初始化

---
+++ FFAnimation/FFAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FFAnimation
+//
+//  Created by 张玲玉 on 2017/2/10.
+//  Copyright © 2017年 bj.zly.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
